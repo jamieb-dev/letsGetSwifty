9dd5f4d51642098dd2d2b1987344ad37eec31ee5
jamieb-dev <jamieb2684@gmail.com>
2023-07-31 20:57:55 +0100

    final commit for archive

---
+++ calculator.py
@@ -0,0 +1,7 @@
+def add(a, b):
+    return a + b
+
+value0 = input("Enter a starting number: ")
+value1 = input("Add another number: ")
+
+print(add(int(value0), int(value1)))
--- calculatorCLI.swift
+++ calculatorCLI.swift
@@ -10,5 +10,8 @@ print("Add another number.")
 var y = readLine()!
 
 var answer = add(a: x, b: y)
+
 print("Adding \(x) and \(y)...")
 print("The answer is \(answer)")
+
+// TASK: Add if..else syntax as seen in temp.swift to accept user input
